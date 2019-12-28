cfcd9b0611daaf89d444e51bec1be5b459165517
Jon Robinson Levy <jon.r.levy@gmail.com>
2019-12-28 11:23:44 +0100

    Adding ReverseVisibilityConverter

---
+++ Xamarin.Forms.Platform.UAP/ReverseVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace Xamarin.Forms.Platform.UWP
+{
+	public class ReverseVisibilityConverter : Windows.UI.Xaml.Data.IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			Visibility v = (Visibility)value;
+
+			return v == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
